5527b4e7ec9e3b551cbeeb64c22ec24d3d407e4e
Tatiana <tstwrt7@gmail.com>
2023-05-04 00:31:10 -0400

    "Exercise 1 - simple_functions"

---
+++ Functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings() {
+    console.log ('My name is Tatiana');
+    console.log('My favorite movie is Fantastic Mr Fox');
+    console.log('My favorite musician is Future');
+}
+
+function whyImHere() {
+console.log("I joined this program so I could build the projects I designed in a UX Design Course that I took last year");
+}
+
+function favoritePlace() {
+console.log("One of my favorite places to visit is New York. I'm from Jersey, so going to the city was a big part of my life and its nostalgic");
+}
+
+console.log(favoriteThings());
+console.log(whyImHere());
+console.log(favoritePlace());
